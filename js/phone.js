cd730b67091752c73cf01747e22a5ea79f657948
Mehedi Hasan <u1904067@student.cuet.ac.bd>
2025-10-02 23:55:52 +0600

    add show all button

--- js/phone.js
+++ js/phone.js
@@ -9,11 +9,25 @@ const loadPhone = async (searchText) => {
 };
 
 const displayPhones = (phones) => {
+    console.log(phones);
     // step 1
   const phonContainer = document.getElementById('phone-container');
 
     // clear phone container cards before adding new cards
     phonContainer.textContent = ' ';
+
+    // display show all button if there are more than 12 phones
+    const showAllContainer = document.getElementById('show-all-container');
+    if(phones.length > 12){
+        showAllContainer.classList.remove('hidden');
+    }
+    else{
+        showAllContainer.classList.add('hidden');
+    }
+
+    // display only first 12 phones
+    phones = phones.slice(0,12);
+    // console.log(phones.length);
 
   phones.forEach((phone) => {
     console.log(phone);
@@ -46,6 +60,12 @@ const handleSearch = () =>{
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value ;
     console.log(searchText);
+    loadPhone(searchText);
+}
+
+const handleSearch2 = () => {
+    const searchField = document.getElementById('search-field2');
+    const searchText = searchField.value;
     loadPhone(searchText);
 }
 
