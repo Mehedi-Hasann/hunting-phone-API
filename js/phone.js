75836ef3f8d5783e81525757f26920a15442cad9
Mehedi Hasan <u1904067@student.cuet.ac.bd>
2025-10-03 01:04:29 +0600

    added the spinner for loading

--- js/phone.js
+++ js/phone.js
@@ -53,20 +53,34 @@ const displayPhones = (phones) => {
     // step 4 : append child 
     phonContainer.appendChild(phoneCard);
   });
-};
+  // hide loading spinner
+  toggleLoadingSpinner(false);
+}
 
 //handle search button
 const handleSearch = () =>{
+    toggleLoadingSpinner(true);
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value ;
     console.log(searchText);
     loadPhone(searchText);
 }
-
+// handle search recap
 const handleSearch2 = () => {
+    toggleLoadingSpinner(true);
     const searchField = document.getElementById('search-field2');
     const searchText = searchField.value;
     loadPhone(searchText);
+}
+
+const toggleLoadingSpinner = (isLoading) => {
+    const loadingSpinner = document.getElementById('loading-spiner');
+    if(isLoading){
+        loadingSpinner.classList.remove('hidden');
+    }
+    else{
+        loadingSpinner.classList.add('hidden');
+    }
 }
 
 // loadPhone();
